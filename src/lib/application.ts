c996f62cae845eee239203025477fa760ce10ade
Panit Tuangsuwan <tpanitte@hotmail.com>
2018-02-09 15:57:39 +0700

    CHANGE: IApplicationComposable

--- src/lib/application.ts
+++ src/lib/application.ts
@@ -4,6 +4,7 @@ import { IProcessContext } from "atomservicescore";
 export namespace Application {
   export interface IApplicationComposable {
     compose?: (composing: Function, ...args: any[]) => Function;
+    useCompose?: (composing: Function, ...args: any[]) => void;
   }
 
   export interface IApplicationListenable {
