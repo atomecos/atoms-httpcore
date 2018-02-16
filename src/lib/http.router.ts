60b7a997053db93cffc3e451457f8993a70f59c4
Panit Tuangsuwan <tpanitte@hotmail.com>
2018-02-16 12:04:14 +0700

    RELEASE: v.0.0.1-alpha.16

--- src/lib/http.router.ts
+++ src/lib/http.router.ts
@@ -1,5 +1,5 @@
 export interface HttpRouter {
   setRoute: (actionPath: string, on: { topic: string; type: string; quality: string; }) => any;
-  setRoutes: (routes: { [key: string]: { topic: string; type: string; quality: string; }; }) => number;
+  setRoutes: (routes: { [actionPath: string]: { topic: string; type: string; quality: string; }; }) => number;
   resolve: (method: string, path: string) => undefined | { channel: { topic: string; type: string; quality: string; }, value: { [name: string]: string; } };
 }
