dfaf8b934a19c1d62b7abf4e6737efbb2daf0af3
Panit Tuangsuwan <tpanitte@hotmail.com>
2018-02-18 10:11:35 +0700

    UPDATE: HttpRouter

--- src/lib/http.router.ts
+++ src/lib/http.router.ts
@@ -3,7 +3,7 @@ import { HttpContext } from "./http.context";
 export interface HttpRouter {
   setRoute(actionPath: string, on: { topic: string; type: string; quality: string; }): any;
   setRoutes(routes: { [actionPath: string]: { topic: string; type: string; quality: string; }; }): number;
-  set404NotFoundHandler(handler: (ctx: HttpContext, next: () => Promise<void>) => Promise<void>): void;
-  set500ErrorHandler(handler: (ctx: HttpContext, next: () => Promise<void>) => Promise<void>): void;
+  set404NotFoundHandler(handler: (ctx: HttpContext, path: string, next: () => Promise<void>) => Promise<void>): void;
+  set500ErrorHandler(handler: (ctx: HttpContext, error: any, next: () => Promise<void>) => Promise<void>): void;
   resolve(method: string, path: string): undefined | { channel: { topic: string; type: string; quality: string; }, value: { [name: string]: string; } };
 }
