f830fb62eb1075fe43f2b6de72a7f5c80b6e86cc
Panit Tuangsuwan <tpanitte@hotmail.com>
2018-02-18 09:55:14 +0700

    CHANGE: set handlers

--- src/lib/http.router.ts
+++ src/lib/http.router.ts
@@ -1,5 +1,9 @@
+import { HttpContext } from "./http.context";
+
 export interface HttpRouter {
   setRoute(actionPath: string, on: { topic: string; type: string; quality: string; }): any;
   setRoutes(routes: { [actionPath: string]: { topic: string; type: string; quality: string; }; }): number;
+  set404NotFoundHandler(handler: (ctx: HttpContext, next: () => Promise<void>) => Promise<void>): void;
+  set500ErrorHandler(handler: (ctx: HttpContext, next: () => Promise<void>) => Promise<void>): void;
   resolve(method: string, path: string): undefined | { channel: { topic: string; type: string; quality: string; }, value: { [name: string]: string; } };
 }
